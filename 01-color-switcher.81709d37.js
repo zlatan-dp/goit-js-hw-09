46d705f9cb27b4d22381f5b216e8f2170cfa45d6
zlatan-dp <99171484+zlatan-dp@users.noreply.github.com>
2022-08-23 09:25:20 +0000

    Deploying to gh-pages from @ zlatan-dp/goit-js-hw-09@e26a764370b9ac7287d2f474bcd72e021ea45d04 🚀

---
+++ 01-color-switcher.81709d37.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),n=document.querySelector("body");let o=null;e.addEventListener("click",(function(){e.disabled=!0,o=setInterval((()=>n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`),1e3)})),t.addEventListener("click",(function(){clearInterval(o),e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.81709d37.js.map
